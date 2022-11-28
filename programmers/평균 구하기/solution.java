84bd6f0b2bbe33c7c8ce4f51d0d6b5ca6aa8411c
shunami <lkn1027@gmail.com>
2022-11-28 16:44:58 +0900

    uploaded at 2022. 11. 28. 오후 4:44:58

---
+++ programmers/평균 구하기/solution.java
@@ -0,0 +1,14 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12944
+
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        int total = 0;
+        
+        for(int num : arr){
+            total +=num;
+        }
+        
+        return answer=(double)total/arr.length;
+    }
+}
