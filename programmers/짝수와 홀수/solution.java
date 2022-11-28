d57ad535d141aadc9398d8b9aae933307a71d7c0
shunami <lkn1027@gmail.com>
2022-11-28 18:29:19 +0900

    Delete programmers directory

--- programmers/짝수와 홀수/solution.java
+++
@@ -1,8 +0,0 @@
-// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12937
-
-class Solution {
-    public String solution(int num) {
-        String answer = ""; 
-        return answer= (num%2==0) ? "Even" : "Odd";   
-    }
-}
