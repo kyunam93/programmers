5d2d36007c6331bf3903a2ef5a17f849bc9f589a
shunami <lkn1027@gmail.com>
2022-11-28 16:44:07 +0900

    uploaded at 2022. 11. 28. 오후 4:44:06

---
+++ programmers/짝수와 홀수/solution.java
@@ -0,0 +1,8 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12937
+
+class Solution {
+    public String solution(int num) {
+        String answer = ""; 
+        return answer= (num%2==0) ? "Even" : "Odd";   
+    }
+}
